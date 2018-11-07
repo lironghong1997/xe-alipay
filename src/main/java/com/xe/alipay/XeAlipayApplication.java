69699b6ece7dda0188e3c0ab7b74a86e2776af1d
lironghong1997 <itlironghong@foxmail.com>
2018-11-07 17:56:13 +0800

    first commit

---
+++ src/main/java/com/xe/alipay/XeAlipayApplication.java
@@ -0,0 +1,12 @@
+package com.xe.alipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XeAlipayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XeAlipayApplication.class, args);
+    }
+}
---
+++ src/main/java/com/xe/alipay/util/MllMapper.java
@@ -0,0 +1,8 @@
+package com.xe.alipay.util;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+public interface MllMapper<T> extends Mapper<T>, MySqlMapper<T> {
+
+}
